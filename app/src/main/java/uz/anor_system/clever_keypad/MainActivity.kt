51d39af271a8d019c5e3f986486895a08888afc8
Qurashboy Erqulov <qurashboy@gmail.com>
2019-07-17 23:29:38 -0700

    added

---
+++ app/src/main/java/uz/anor_system/clever_keypad/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.anor_system.clever_keypad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
